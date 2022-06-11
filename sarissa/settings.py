54e4cdb5b5d6aa803521c269729be224ad2ca78e
SuleimanMS2 <96366085+SuleimanMS2@users.noreply.github.com>
2022-06-11 09:46:49 +0500

    Update settings.py

--- sarissa/settings.py
+++ sarissa/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-j6v5(swaf%0g5q!vrfe(((7lp$ma(*n!r2xqr%s*g07)7s+u($
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sarissa.uz']
 
 # Application definition
 
@@ -81,15 +81,14 @@ WSGI_APPLICATION = 'sarissa.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -100,7 +99,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
